8fb02b2321a0fd0ec22a4a4b1501df1d9877ecc4
resteve <resteve@zikzakmedia.com>
2013-09-16 11:42:31 +0200

    Create esale product

--- product.py
+++ product.py
@@ -2,8 +2,11 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.model import fields, ModelSQL
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
+from trytond.transaction import Transaction
+
+import logging
 
 __all__ = ['Template', 'TemplateSaleShop']
 __metaclass__ = PoolMeta
@@ -27,6 +30,40 @@ class Template:
                 ('esale_available', '=', True)
             ],
             help='Select shops will be available this product')
+
+
+    @classmethod
+    def create_esale_product(self, shop, tvals, pvals):
+        '''
+        Get product values and create
+        :param shop: obj
+        :param tvals: dict template values
+        :param pvals: dict product values
+        return obj
+        '''
+        Template = Pool().get('product.template')
+
+        #Default values
+        tvals['esale_available'] = True
+        tvals['esale_active'] = True
+        tvals['default_uom'] = shop.esale_uom_product
+        tvals['category'] = shop.esale_category
+        tvals['salable'] = True
+        tvals['sale_uom'] = shop.esale_uom_product
+        tvals['account_category'] = True
+        tvals['products'] = [('create', [pvals])]
+
+        if tvals.get('esale_saleshops'):
+            shops = tvals.get('esale_saleshops')
+            del tvals['esale_saleshops']
+
+        template = Template.create([tvals])[0]
+        Transaction().cursor.commit()
+        if shops:
+            Template.write([template], {'esale_saleshops': shops})
+        logging.getLogger('magento sale').info(
+            'Magento %s. Create product %s' % (shop.name, pvals['code']))
+        return template.products[0]
 
 
 class TemplateSaleShop(ModelSQL):
