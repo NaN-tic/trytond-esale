d110befc8a08dad2a8d00c4424b96047553597b9
jmartin <jmartin@zikzakmedia.com>
2014-06-20 13:41:54 +0200

    Added method to compute the sale price of the product depending of user and shop configuration

--- product.py
+++ product.py
@@ -20,6 +20,68 @@ class Template:
                 ('esale_available', '=', True)
             ],
             help='Select shops will be available this product')
+    esale_price = fields.Function(fields.Numeric('eSale Price',
+            digits=(16, 4)), 'get_esale_price')
+
+    @classmethod
+    def get_esale_price(cls, templates, names):
+        pool = Pool()
+        Product = pool.get('product.product')
+        User = pool.get('res.user')
+        Shop = pool.get('sale.shop')
+
+        def template_list_price():
+            return {n: {t.id: t.list_price for t in templates} for n in names}
+
+        def pricelist():
+            products = [p for t in templates for p in t.products]
+            sale_prices = Product.get_sale_price(products
+                )
+            prices = {}
+            for price in sale_prices:
+                for template in templates:
+                    prices[template.id] = None
+                    for p in template.products:
+                        if (p.id == price and (
+                                not prices[template.id]
+                                or prices[template.id] > sale_prices[price])):
+                            prices[template.id] = sale_prices[price]
+            return {n: {t.id: prices[t.id] for t in templates} for n in names}
+
+        def price_with_tax(result):
+            for name in names:
+                for t in templates:
+                    for p in t.products:
+                        result[name][t.id] = Shop.esale_price_w_taxes(p,
+                            result[name][t.id])
+                        break
+            return result
+
+        if (Transaction().user == 0
+                and Transaction().context.get('user')):
+            user = User(Transaction().context.get('user'))
+        else:
+            user = User(Transaction().user)
+        shop = user.shop
+        if not shop:
+            logging.getLogger('esale').warning(
+                'User %s has not any shop associated.' % (user))
+            return template_list_price()
+        context = {}
+        if shop.esale_price == 'pricelist':
+            context['price_list'] = shop.price_list.id
+        if shop.price_list:
+            context['customer'] = shop.esale_price_party.id
+        if shop.esale_price_party:
+            context['without_special_price'] = True
+        with Transaction().set_context(context):
+            if shop.esale_available and shop.esale_price == 'saleprice':
+                result = template_list_price()
+            else:
+                result = pricelist()
+            if shop.esale_tax_include:
+                result = price_with_tax(result)
+            return result
 
     @staticmethod
     def default_esale_saleshops():
