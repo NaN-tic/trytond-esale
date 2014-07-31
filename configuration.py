694d3c09cdd1c1b4da4b46cd9620d1aa890a8fd9
Jordi Esteve <jesteve@zikzakmedia.com>
2014-07-31 15:59:52 +0200

    Change sale configuration fields to properties, to allow delete foreign records. The sale_configuration table must be updated manually: alter table sale_configuration drop column sale_category; alter table sale_configuration drop column sale_currency; alter table sale_configuration drop column sale_discount_product; alter table sale_configuration drop column sale_surcharge_product; alter table sale_configuration drop column sale_delivery_product; alter table sale_configuration drop column sale_uom_product; alter table sale_configuration drop column sale_price_list; alter table sale_configuration drop column sale_warehouse; alter table sale_configuration drop column sale_payment_type; alter table sale_configuration drop column sale_payment_term;

--- configuration.py
+++ configuration.py
@@ -1,5 +1,5 @@
 #This file is part esale module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
+#The COPYRIGHT file at the top level of this repository contains
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
@@ -11,23 +11,26 @@ __metaclass__ = PoolMeta
 class Configuration:
     'Sale Configuration'
     __name__ = 'sale.configuration'
-    sale_delivery_product = fields.Many2One('product.product',
-        'Delivery Product', required=True)
-    sale_discount_product = fields.Many2One('product.product',
-        'Discount Product', required=True)
-    sale_surcharge_product = fields.Many2One('product.product',
-        'Surcharge Product', required=True)
-    sale_uom_product = fields.Many2One('product.uom', 'Default UOM',
-        required=True)
-    sale_warehouse = fields.Many2One('stock.location', 'Default Warehouse',
-        domain=[('type', '=', 'warehouse')], required=True)
-    sale_payment_type = fields.Many2One('account.payment.type',
-        'Default Payment Type', domain=[('kind', '=', 'receivable')], required=True)
-    sale_payment_term = fields.Many2One('account.invoice.payment_term',
-        'Default Payment Term', required=True)
-    sale_price_list = fields.Many2One('product.price_list',
-        'Default Price List', required=True)
-    sale_currency = fields.Many2One('currency.currency', 'Currency',
-        required=True)
-    sale_category = fields.Many2One('product.category', 'Product Category',
-        required=True)
+    sale_delivery_product = fields.Property(fields.Many2One('product.product',
+        'Delivery Product', required=True))
+    sale_discount_product = fields.Property(fields.Many2One('product.product',
+        'Discount Product', required=True))
+    sale_surcharge_product = fields.Property(fields.Many2One('product.product',
+        'Surcharge Product', required=True))
+    sale_uom_product = fields.Property(fields.Many2One('product.uom',
+        'Default UOM', required=True))
+    sale_warehouse = fields.Property(fields.Many2One('stock.location',
+        'Default Warehouse', domain=[('type', '=', 'warehouse')],
+        required=True))
+    sale_payment_type = fields.Property(fields.Many2One('account.payment.type',
+        'Default Payment Type', domain=[('kind', '=', 'receivable')],
+        required=True))
+    sale_payment_term = fields.Property(
+        fields.Many2One('account.invoice.payment_term',
+        'Default Payment Term', required=True))
+    sale_price_list = fields.Property(fields.Many2One('product.price_list',
+        'Default Price List', required=True))
+    sale_currency = fields.Property(fields.Many2One('currency.currency',
+        'Currency', required=True))
+    sale_category = fields.Property(fields.Many2One('product.category',
+        'Product Category', required=True))
