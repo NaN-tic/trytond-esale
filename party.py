f70dd4c79e4e97264502bdf8cd5fbd5de6624146
resteve <resteve@zikzakmedia.com>
2015-10-01 22:52:47 +0200

    Create Party. refactore method with identifier search vat code

--- party.py
+++ party.py
@@ -4,20 +4,14 @@
 from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
 from trytond.pyson import Eval
-from trytond.modules.esale.tools import is_a_vat
+import stdnum.eu.vat as vat
 import logging
-
-logger = logging.getLogger(__name__)
-
-try:
-    import vatnumber
-    HAS_VATNUMBER = True
-except ImportError:
-    logger.warning(
-            'Unable to import vatnumber. VAT number validation disabled.')
 
 __all__ = ['Party']
 __metaclass__ = PoolMeta
+
+logger = logging.getLogger(__name__)
+_ESALE_PARTY_EXCLUDE_FIELDS = ['vat_country', 'vat_code']
 
 
 class Party:
@@ -41,53 +35,44 @@ class Party:
         '''
         pool = Pool()
         Party = pool.get('party.party')
+        Identifier = pool.get('party.identifier')
         ContactMechanism = pool.get('party.contact_mechanism')
 
-        # Validate VAT
-        if (values.get('vat_country') and values.get('vat_number') and
-                HAS_VATNUMBER):
-            vat_number = values.get('vat_number')
-            vat_country = values.get('vat_country')
+        vat_code = values.get('vat_code')
+        vat_country = values.get('vat_country')
 
-            vat_number = '%s%s' % (vat_country.upper(), vat_number)
-            if not vatnumber.check_vat(vat_number):
-                del values['vat_country']
-        elif 'vat_country' in values:
-            del values['vat_country']
-
-        if 'country' in values and not values.get('country'):
-            del values['country']
+        is_vat = False
+        if vat_country and vat_code:
+            code = '%s%s' % (vat_country.upper(), vat_code)
+            if vat.is_valid(code):
+                vat_code = code
+                is_vat = True
 
         #  Search party by:
-        #  - Vat country + vat number
-        #  - Vat number
+        #  - VAT country + VAT code
+        #  - VAT code
         #  - Party eSale Email
         #  - Party Email
         party = None
-        if shop.esale_get_party_by_vat:
-            vat_number = values.get('vat_number')
-            if (vat_number and is_a_vat(vat_number) and
-                    values.get('vat_country')):
+        # search by VAT
+        if shop.esale_get_party_by_vat and vat_code:
+            parties = Party.search([
+                ('identifier_code', '=', vat_code),
+                ], limit=1)
+            if not parties and is_vat:
                 parties = Party.search([
-                    ('vat_country', '=', values.get('vat_country')),
-                    ('vat_number', '=', values.get('vat_number')),
+                    ('identifier_code', '=', vat_code[2:]),
                     ], limit=1)
-                if parties:
-                    party, = parties
-            if vat_number and is_a_vat(vat_number) and not party:
-                parties = Party.search([
-                    ('vat_number', '=', values.get('vat_number')),
-                    ], limit=1)
-                if parties:
-                    party, = parties
-
+            if parties:
+                party, = parties
+        # search by esale email
         if not party:
             parties = Party.search([
                 ('esale_email', '=', values.get('esale_email')),
                 ], limit=1)
             if parties:
                 party, = parties
-
+        # search by mechanism email
         if not party:
             mechanisms = ContactMechanism.search([
                 ('type', '=', 'email'),
@@ -97,9 +82,21 @@ class Party:
                 mechanism, = mechanisms
                 party = mechanism.party
 
+        # not found, create
         if not party:
-            values['addresses'] = None
-            party, = Party.create([values])
-            logger.info('Shop %s. Create party ID %s' % (
+            party = Party()
+            for k, v in values.iteritems():
+                if k not in _ESALE_PARTY_EXCLUDE_FIELDS:
+                    setattr(party, k, v)
+            party.addresses = None
+            if vat_code:
+                identifier = Identifier()
+                identifier.code = vat_code
+                if is_vat:
+                    identifier.type = 'eu_vat'
+                party.identifiers = [identifier]
+            party.save()
+
+            logger.info('Shop %s. Created party ID %s' % (
                 shop.name, party.id))
         return party
