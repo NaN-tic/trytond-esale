fb21fcaed8558bf41d37214d6f49004ea3d252d0
resteve <resteve@zikzakmedia.com>
2015-12-01 11:07:29 +0100

    Tests

--- tests/test_esale.py
+++ tests/test_esale.py
@@ -1,46 +1,18 @@
-#!/usr/bin/env python
-#This file is part esale module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part of the esale module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class eSaleTestCase(unittest.TestCase):
-    '''
-    Test eSale module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('esale')
-
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('esale')
-
-    def test0006depends(self):
-        '''
-        Test depends.
-        '''
-        test_depends()
+class EsaleTestCase(ModuleTestCase):
+    'Test Esale module'
+    module = 'esale'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        eSaleTestCase))
+        EsaleTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
