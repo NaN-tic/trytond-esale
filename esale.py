69797502652fae082d6db52f1412aef024d9f72a
Raimon Esteve <raimon@nan-tic.com>
2018-03-01 12:06:19 +0100

    Allow compute esale tax rule without subdivision

--- esale.py
+++ esale.py
@@ -136,12 +136,12 @@ class eSaleAccountTaxRule(ModelSQL, ModelView, MatchMixin):
             self.subdivision = None
 
     @classmethod
-    def compute(cls, country, subdivision, zip, pattern=None):
+    def compute(cls, country, subdivision=None, zip=None, pattern=None):
         'Compute esale account tax rule based on party address'
-        etax_rules = cls.search([
-            ('country', '=', country),
-            ('subdivision', '=', subdivision),
-            ])
+        domain = [('country', '=', country)]
+        if subdivision:
+            domain.append(('subdivision', '=', subdivision))
+        etax_rules = cls.search(domain)
         if pattern is None:
             pattern = {}
 
