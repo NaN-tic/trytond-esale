2a2367b8e630a8ea199b1e200ce1f1b6e7f3e94d
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-05-16 09:34:09 +0200

    Replace CompanyTestMixin to SaleShopCompanyTestMixin
    Task #048600

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,11 +2,11 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 
-from trytond.modules.company.tests import CompanyTestMixin
+from trytond.modules.sale_shop.tests import SaleShopCompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class EsaleTestCase(CompanyTestMixin, ModuleTestCase):
+class EsaleTestCase(SaleShopCompanyTestMixin, ModuleTestCase):
     'Test Esale module'
     module = 'esale'
     extras = ['sale_discount', 'sale_pos', 'sale_stock_quantity', 'stock_delivery']
