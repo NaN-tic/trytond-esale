423c01e36a1396e93990601f6ef800c3fe1574fa
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 11:47:35 +0200

    Pyflakes fixes

--- configuration.py
+++ configuration.py
@@ -1,7 +1,6 @@
 # This file is part esale module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond import backend
 from trytond.model import ModelSQL, ValueMixin, fields
 from trytond.pool import Pool, PoolMeta
 
